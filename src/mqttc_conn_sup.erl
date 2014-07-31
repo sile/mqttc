91802badae15fd3a24ffe718238f48a05fb902d9
Takeru Ohta <phjgt308@gmail.com>
2014-08-01 04:36:43 +0900

    Add supervisor module for mqttc_conn

--- src/mqttc_conn_sup.erl
+++ src/mqttc_conn_sup.erl
@@ -1,8 +1,8 @@
 %% @copyright 2014 Takeru Ohta <phjgt308@gmail.com>
 %%
-%% @doc application supervisor module
+%% @doc supervisor for client MQTT connection processes
 %% @private
--module(mqttc_sup).
+-module(mqttc_conn_sup).
 
 -behaviour(supervisor).
 
@@ -10,6 +10,8 @@
 %% Exported API
 %%------------------------------------------------------------------------------------------------------------------------
 -export([start_link/0]).
+-export([start_child/1]).
+-export([which_children/0]).
 
 %%------------------------------------------------------------------------------------------------------------------------
 %% 'application' Callback API
@@ -24,16 +26,22 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+%% @doc Starts child process
+-spec start_child(mqttc_connection:start_arg()) -> {ok, pid()} | {error, Reason} when
+      Reason :: {already_started, pid()} | term().
+start_child(Arg) ->
+    supervisor:start_child(?MODULE, [Arg]).
+
+-spec which_children() -> [pid()].
+which_children() ->
+    [Pid || {_, Pid, _, _} <- supervisor:which_children(?MODULE)].
+
 %%------------------------------------------------------------------------------------------------------------------------
 %% 'application' Callback Functions
 %%------------------------------------------------------------------------------------------------------------------------
 %% @private
 init([]) ->
-    ConnectionSup = mqttc_conn_sup,
-    SessionSup = mqttc_session_sup,
+    Connection = mqttc_conn,
     Children =
-        [
-         {ConnectionSup, {ConnectionSup, start_link, []}, permanent, 5000, supervisor, [ConnectionSup]},
-         {SessionSup, {SessionSup, start_link, []}, permanent, 5000, supervisor, [SessionSup]}
-        ],
-    {ok, { {one_for_one, 5, 10}, Children} }.
+        [{Connection, {Connection, start_link, []}, temporary, 5000, worker, [Connection]}],
+    {ok, { {simple_one_for_one, 5, 10}, Children} }.
